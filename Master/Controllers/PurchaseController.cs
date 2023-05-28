bda97c8758185b4b287d878baa60f4ac31535af5
Raju <rajuoffice220@gmail.com>
2023-05-28 15:51:46 +0600

    Js, Controller and View Complete

--- Master/Controllers/PurchaseController.cs
+++ Master/Controllers/PurchaseController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -18,14 +18,14 @@ namespace MasterDetails.Controllers
             _context = context;
         }
 
-        // GET: Purchases
+        // GET: Purchase
         public async Task<IActionResult> Index()
         {
             var appDbContext = _context.Purchase.Include(p => p.Supplier);
             return View(await appDbContext.ToListAsync());
         }
 
-        // GET: Purchases/Details/5
+        // GET: Purchase/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Purchase == null)
@@ -44,124 +44,124 @@ namespace MasterDetails.Controllers
             return View(purchase);
         }
 
-        // GET: Purchases/Create
+        // GET: Purchase/Create
         public IActionResult Create()
         {
-            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "Id");
-            return View();
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "SupplierName");
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "ProductName");
+            Purchase purchase = new Purchase();
+            purchase.PurchaseProducts.Add(new PurchaseProduct { Id = 1 });
+            return View(purchase);
         }
 
-        // POST: Purchases/Create
+        // POST: Purchase/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,PurchaseDate,PurchaseCode,PurchaseType,SupplierId,TotalAmount,DiscountPercentage,DiscountAmount,VatPercentage,VatAmount,PaymentAmount")] Purchase purchase)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && purchase.SupplierId == 0)
             {
-                _context.Add(purchase);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "SupplierName");
+                ViewData["ProductId"] = new SelectList(_context.Products, "Id", "ProductName");
+                return View(purchase);
+
             }
-            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "Id", purchase.SupplierId);
-            return View(purchase);
+            purchase.PurchaseProducts.RemoveAll(p => p.Quantity == 0);
+            _context.Add(purchase);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
 
-        // GET: Purchases/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+
+        // GET: Purchase/Edit/5
+        public IActionResult Edit(int? id)
         {
-            if (id == null || _context.Purchase == null)
-            {
-                return NotFound();
-            }
 
-            var purchase = await _context.Purchase.FindAsync(id);
-            if (purchase == null)
-            {
-                return NotFound();
-            }
-            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "Id", purchase.SupplierId);
+
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "SupplierName");
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "ProductName");
+
+            Purchase purchase = _context.Purchase.Where(x => x.Id == id)
+                .Include(i => i.PurchaseProducts)
+                .ThenInclude(i => i.Product)
+                .FirstOrDefault();
+
+            purchase.PurchaseProducts.ForEach(x => x.Amount = x.Quantity * x.PurchasePrice);
+
             return View(purchase);
         }
 
-        // POST: Purchases/Edit/5
+        // POST: Purchase/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,PurchaseDate,PurchaseCode,PurchaseType,SupplierId,TotalAmount,DiscountPercentage,DiscountAmount,VatPercentage,VatAmount,PaymentAmount")] Purchase purchase)
         {
-            if (id != purchase.Id)
+            purchase.PurchaseProducts.RemoveAll(p => p.Quantity == 0);
+
+            try
             {
-                return NotFound();
+                List<PurchaseProduct> purchaseItems = _context.PurchaseProducts.Where(p => p.Id == id).ToList();
+                _context.PurchaseProducts.RemoveRange(purchaseItems);
+                await _context.SaveChangesAsync();
+                _context.Attach(purchase);
+                _context.Entry(purchase).State = EntityState.Modified;
+                _context.PurchaseProducts.AddRange(purchase.PurchaseProducts);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
             }
 
-            if (ModelState.IsValid)
+            catch (Exception ex)
             {
-                try
-                {
-                    _context.Update(purchase);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PurchaseExists(purchase.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                throw;
             }
-            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "Id", purchase.SupplierId);
-            return View(purchase);
         }
 
-        // GET: Purchases/Delete/5
+        // GET: Purchase/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Purchase == null)
-            {
-                return NotFound();
-            }
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "Id", "SupplierName");
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "ProductName");
+            Purchase purchase = _context.Purchase.Where(p => p.Id == id)
+                .Include(p => p.PurchaseProducts)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefault();
 
-            var purchase = await _context.Purchase
-                .Include(p => p.Supplier)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (purchase == null)
-            {
-                return NotFound();
-            }
-
+            purchase.PurchaseProducts.ForEach(p => p.Amount = p.Quantity * p.PurchasePrice);
             return View(purchase);
         }
 
-        // POST: Purchases/Delete/5
+        // POST: Purchase/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(Purchase purchase)
         {
-            if (_context.Purchase == null)
+            try
             {
-                return Problem("Entity set 'AppDbContext.Purchase'  is null.");
-            }
-            var purchase = await _context.Purchase.FindAsync(id);
-            if (purchase != null)
-            {
-                _context.Purchase.Remove(purchase);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+                List<PurchaseProduct> purchaseItems = _context.PurchaseProducts.Where(i => i.PurchaseId == purchase.Id).ToList();
+                _context.PurchaseProducts.RemoveRange(purchaseItems);
+                await _context.SaveChangesAsync();
+                _context.Remove(purchase);
+                await _context.SaveChangesAsync();
 
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
         private bool PurchaseExists(int id)
         {
-          return _context.Purchase.Any(e => e.Id == id);
+            return _context.Purchase.Any(e => e.Id == id);
         }
+
     }
 }
